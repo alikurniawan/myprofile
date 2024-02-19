d208b9361d8327da6733ccc32de0117f97478b64
alikurniawan <alikoerniawan02@gmail.com>
2024-02-19 23:40:05 +0700

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import styles from '@/styles/globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,6 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div>
           ALI KURNIAWAN
+          <button>test</button>
         </div>
       </main>
     </>
