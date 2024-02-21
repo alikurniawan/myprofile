53b43ccd921d179933141e9628b9a6d264055f2e
alikurniawan <alikoerniawan02@gmail.com>
2024-02-21 12:47:42 +0700

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
+import Footer from '@/components/footer'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  )
+
 }
